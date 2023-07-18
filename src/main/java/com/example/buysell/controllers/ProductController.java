35a13b56e23e5d4b148ef7d2ae77442c1a400fc7
Kirill Lagutin <lagutin.k@mail.ru>
2023-07-18 22:52:37 +0300

    Initial commit

---
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -0,0 +1,42 @@
+package com.example.buysell.controllers;
+
+import com.example.buysell.models.Product;
+import com.example.buysell.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/")
+    public String products(@RequestParam(name = "title", required = false) String title, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
+        return "products";
+    }
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+
+        return "product-info";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct(Product product) {
+        productService.saveProduct(product);
+        return "redirect:/";
+    }
+
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -0,0 +1,37 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.Product;
+import com.example.buysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> listProducts(String title) {
+        List<Product> products = productRepository.findAll();
+        if (title != null) {
+            return productRepository.findByTitle(title);
+        }
+        return productRepository.findAll();
+    }
+
+    public void saveProduct(Product product) {
+        log.info("Saving new {}", product);
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
